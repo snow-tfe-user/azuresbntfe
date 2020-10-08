244aa6043f921f14b0574674461581c09a1e08ed
snow-tfe-user <69755779+snow-tfe-user@users.noreply.github.com>
2020-10-08 18:00:04 +0530

    Update sbntemplate.tf

--- sbntemplate.tf
+++ sbntemplate.tf
@@ -1,22 +1,28 @@
-resource "random_id" "server" {
-  byte_length = 8
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "azsbnrg-${random_id.server.hex}"
+  name     = "${var.rgname}"
   location = "${var.region}"
 }
 
-resource "azurerm_virtual_network" "nw" {
-  name                = "network-${random_id.server.hex}"
+resource "azurerm_virtual_network" "db_app_nw" {
+  count               = 1
+  name                = "db_app_nw-${format("%00.0f", count.index)}-${substr(azurerm_resource_group.rg.name,11,14)}"
   address_space       = ["10.0.0.0/16"]
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 }
 
-resource "azurerm_subnet" "sbnt" {
-  name                 = "subnet-${random_id.server.hex}"
+resource "azurerm_subnet" "db_sbnt" {
+  count                = 1
+  name                 = "db_sbnt-${format("%00.0f", count.index)}-${substr(azurerm_resource_group.rg.name,11,14)}"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.nw.name}"
-  address_prefix       = "10.0.2.0/24"
+  virtual_network_name = "${azurerm_virtual_network.db_app_nw.name}"
+  address_prefix       = "10.0.0.0/24"
+}
+
+resource "azurerm_subnet" "app_sbnt" {
+  count                = 1
+  name                 = "app_sbnt-${format("%00.0f", count.index)}-${substr(azurerm_resource_group.rg.name,11,14)}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.db_app_nw.name}"
+  address_prefix       = "10.0.1.0/24"
 }
