c632dd4eb226e7da0101426715f47863b9b15076
snow-tfe-user <69755779+snow-tfe-user@users.noreply.github.com>
2020-10-08 18:29:12 +0530

    Update sbntemplate.tf

--- sbntemplate.tf
+++ sbntemplate.tf
@@ -15,7 +15,7 @@ resource "azurerm_subnet" "db_sbnt" {
   count                = 1
   name                 = "db_sbnt-${format("%00.0f", count.index)}-${substr(azurerm_resource_group.rg.name,11,14)}"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.db_app_nw.name}"
+  virtual_network_name = "${azurerm_virtual_network.db_app_nw[count.index].name}"
   address_prefix       = "10.0.0.0/24"
 }
 
@@ -23,6 +23,6 @@ resource "azurerm_subnet" "app_sbnt" {
   count                = 1
   name                 = "app_sbnt-${format("%00.0f", count.index)}-${substr(azurerm_resource_group.rg.name,11,14)}"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.db_app_nw.name}"
+  virtual_network_name = "${azurerm_virtual_network.db_app_nw[count.index].name}"
   address_prefix       = "10.0.1.0/24"
 }
